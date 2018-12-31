2d7187e4f3fac014aecb81ce75dc06e78494a356
george <george>
2018-12-31 10:27:38 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,94 @@
+# ABCD 2-port functions for phasing vertical antenna arrays. 
+
+The examples are taken from Gehrke K2BT's "Vertical Phased Arrays"
+series in Ham Radio Magazine, May-July 1983, October 1983, December 1983.
+
+The first group of functions calculate the ABCD matrix.
+The order of the components are entered in order from the
+source side to the load side.
+
+```
+auto(ratio, xt, k=1):       an autotransformer; 1:n is the ratio
+mutual(n, x1, k=1):         a transformer; 1:n is the ratio
+trans(n):                   an ideal transformer; 1:n is the ratio
+series(z):                  a component in series
+shunt(y):                   a component in parallel
+halfpi(y, z):               a shunt input l-match
+halftee(z, y):              a series input l-match
+tline(deg, zo=50):          a transmission line z of length deg
+fulltee(z1, z2, z3):        a tee section
+fullpi(z1, z2, z3):         a pi section
+halftee2(z1, y1, z2, y2):   a double series input l-match
+halfpi2(z1, y1, z2, y2):    a double shunt input l-match
+```
+
+The second group of functions solve various impedance matching problems.
+The solution parameter changes the components to their compliments.
+
+```
+to_halfpi(rin, za, solution=0):     match with a shunt input l-match
+to_halftee(rin, za, solution=0):    match with a series input l-match
+to_halfwave(zs, za, solution=0):    match with a 90 degree tee/pi section
+to_shunt(za):                       cancel reactance with a shunt section
+to_series(za):                      cancel reactance with a series section
+to_fullpi(deg, zo):                 shift phase with a pi section
+to_fulltee(deg, zo):                      shift phase with a tee section
+to_halfpi2(zs, za, solution=(0,0)):       match with a double shunt l-match
+to_halftee2(zs, za, solution=(0,0)):      match with a double series l-match
+to_fullpi2(zs, za, q=0, solution=(0,0)):  match with a pi section
+to_fulltee2(zs, za, q=0, solution=(0,0)): match with a tee section
+```
+
+The third group of functions are basically helper functions
+
+```
+def qmin(zs, za):
+def qmin2(zs, za):
+def openstub(deg, zo):
+def shortstub(deg, zo):
+def reactance_value(component, fd):
+def component_value(impedance, fd):
+def parallel(*impedances):
+def vec(e, i):
+def emag(v):
+def ephase(v):
+def power(*vectors):
+def impedance(*vectors):
+```
+
+The fourth group of functions are for printing results:
+
+```
+def polar(x):
+def status(v, note=0):
+def component(values, fd, precision=4):
+def notation(x, precision=4):
+```
+
+# Example
+
+For example, with a load with 1 amp and 5 volts going through it, transform
+the load with 90 degrees of 50 ohms transmission line.
+
+
+```
+from abcd import *
+line = vec(50, 1)
+line = tline(135) * line
+status(line)
+```
+
+Gives
+
+```
+p(0) = 50.0000
+z(0) = 50.0000+0.0000j
+i(0) = 1.0000 / 135.0000
+e(0) = 50.0000 / 135.0000
+```
+
+
+
+
+
+
---
+++ abcd.py
@@ -0,0 +1,228 @@
+import numpy as np
+
+# generates ABCD matrix
+######################################
+
+def auto(ratio, xt, k=1):
+    n = ratio / (1 - ratio)
+    x1 = xt / (1 + n**2 + 2 * k * n)
+    x2 = x1 * n**2
+    xm = k * n * x1
+    return fulltee((x1 + xm) * 1j, (x2 + xm) * 1j, -xm * 1j)
+
+def mutual(n, x1, k=1):
+    x2 = x1 * n**2
+    xm = k * n * x1
+    return fulltee((x1 - xm) * 1j, xm * 1j, (x2 - xm) * 1j)
+
+def trans(n):
+    return np.matrix([[ 1/n, 0], [0, n]])
+    
+# <---Z---< ZL
+def series(z):
+    return np.matrix([[1, z], [0, 1]])
+
+# <---+---< ZL
+#     Y
+def shunt(y):
+    return np.matrix([[1, 0], [1/y, 1]])
+
+# <---+---Z---< ZL
+#     Y
+def halfpi(y, z):
+    return np.matrix([
+        [ 1, z ],
+        [ 1/y , 1+z/y ]
+    ])
+
+# <---Z---+---<  ZL
+#         Y
+def halftee(z, y):
+    return np.matrix([
+        [ 1+z/y, z ],
+        [ 1/y , 1 ]
+    ])
+
+# <----O=======O---< ZL
+def tline(deg, zo=50):
+    theta = float(deg) * np.pi / 180
+    return np.matrix([
+        [ np.cos(theta), 1j*zo*np.sin(theta)],
+        [ 1j*np.sin(theta)/zo, np.cos(theta) ]
+    ])
+
+# <---Z1---+---Z3---< ZL
+#          z2     
+def fulltee(z1, z2, z3):
+    return np.matrix([
+        [ 1 + z1/z2, z1 + z3 + z1*z3/z2 ],
+        [ 1/z2, 1 + z3/z2 ],
+    ])
+
+# <---+---Z2---+---< ZL
+#     Z1       Z3
+def fullpi(z1, z2, z3):
+    return np.matrix([
+        [ 1 + z2/z3, z2 ],
+        [ (z1 + z2 + z3) / (z1*z3), 1 + z2/z1 ]
+    ])
+
+def halftee2(z1, y1, z2, y2):
+    return halftee(z1, y1) * halftee(z2, y2)
+
+def halfpi2(z1, y1, z2, y2):
+    return halfpi(z1, y1) * halfpi(z2, y2)
+
+# circuit solutions
+######################################
+
+# it keeps the phase the same!
+def to_halfwave(zs, za, solution=0):
+    r1, r2 = zs.real, za.real
+    x = np.sqrt(r1 * r2) * 1j
+    if solution: x = -x
+    return x, -x, x
+    
+def to_halfpi(rin, za, solution=0):
+    ra, xa = za.real, za.imag
+    xd = np.sqrt(ra * (rin - ra))
+    if np.iscomplex(xd): raise ValueError
+    x2 = np.array([-xa - xd, -xa + xd])
+    x2 = x2[solution]
+    x1 = -(ra**2 + (x2 + xa)**2) / (x2 + xa)
+    return x1 * 1j, x2 * 1j
+
+def to_halftee(rin, za, solution=0):
+    ra, xa = za.real, za.imag
+    xd = np.sqrt(rin * ra * (ra**2 + xa**2 - rin * ra))
+    if np.iscomplex(xd): raise ValueError
+    x2 = np.array([(-rin * xa + xd) / (rin - ra),
+                   (-rin * xa - xd) / (rin - ra)])
+    x2 = x2[solution]
+    x1 = -x2 * (ra**2 + xa * (x2 + xa)) / (ra**2 + (x2 + xa)**2)
+    return x1 * 1j, x2 * 1j
+
+def to_fullpi(deg, zo):
+    zo = zo.real
+    theta = float(deg) * np.pi / 180
+    x2 = zo * np.sin(theta)
+    x1 = -zo * np.sin(theta) / (1 - np.cos(theta))
+    return x1 * 1j, x2 * 1j, x1 * 1j
+
+def to_fulltee(deg, zo):
+    zo = zo.real
+    theta = float(deg) * np.pi / 180
+    x2 = -zo / np.sin(theta)
+    x1 = zo * (1 - np.cos(theta)) / np.sin(theta)
+    return x1 * 1j, x2 * 1j, x1 * 1j
+
+def to_halfpi2(zs, za, solution=(0,0)):
+    rin = np.sqrt(zs.real * za.real)
+    x = to_halftee(rin, zs, solution=solution[0])
+    y = to_halfpi(rin, za, solution=solution[1])
+    return x[1], x[0], y[0], y[1]
+
+def to_halftee2(zs, za, solution=(0,0)):
+    rin = np.sqrt(zs.real * za.real)
+    x = to_halfpi(rin, zs, solution=solution[0])
+    y = to_halftee(rin, za, solution=solution[1])
+    return x[1], x[0], y[0], y[1]
+    
+def to_fullpi2(zs, za, q=0, solution=(0,0)):
+    q = q or qmin(zs, za) + 1e-9
+    rin = max(zs.real, za.real) / (q**2 + 1)
+    x = to_halftee(rin, zs, solution=solution[0])
+    y = to_halftee(rin, za, solution=solution[1])
+    return x[1], x[0] + y[0], y[1]
+
+def to_fulltee2(zs, za, q=0, solution=(0,0)):
+    q = q or qmin(zs, za) + 1e-9
+    rin = min(zs.real, za.real) * (q**2 + 1)
+    x = to_halfpi(rin, zs, solution=solution[0])
+    y = to_halfpi(rin, za, solution=solution[1])
+    return x[1], parallel(x[0], y[0]), y[1]
+     
+def to_shunt(za):
+    ra, xa = za.real, za.imag
+    x1 = -(xa + ra**2 / xa)
+    return (x1 * 1j,)
+
+def to_series(za):
+    x1 = -za.imag
+    return (x1 * 1j,)
+
+######################################
+
+# q for L network, or minimum q for tee or pi
+def qmin(zs, za):
+    rp = max(zs.real, za.real)
+    rs = min(zs.real, za.real)
+    return np.sqrt(rp / rs - 1)
+
+# minimum q for a ll network
+def qmin2(zs, za):
+    rp = max(zs.real, za.real)
+    rv = np.sqrt(rp * rs)
+    return np.sqrt(rp / rv - 1)
+
+def openstub(deg, zo):
+    theta = float(deg) * np.pi / 180
+    return -1j * zo * np.cot(theta)
+
+def shortstub(deg, zo):
+    theta = float(deg) * np.pi / 180
+    return 1j * zo * np.tan(theta)
+
+def reactance_value(component, fd):
+    w = 2 * np.pi * fd
+    return 1j / (w * component) if component < 0 else 1j * w * component
+
+def component_value(impedance, fd):
+    x = impedance.imag
+    w = 2 * np.pi * fd
+    return 1 / (w * x) if x < 0 else x / w  
+
+def parallel(*impedances):
+    return 1 / sum(1 / x for x in impedances)
+
+# for line
+
+def vec(e, i):
+    return np.matrix([complex(e), complex(i)]).T
+
+def emag(v):
+    return float(np.absolute(v[0]))
+
+def ephase(v):
+    return float(np.angle(v[0], deg=True))
+
+def power(*vectors):
+    return sum(float(np.absolute(v[1])**2 * impedance(v).real) for v in vectors)
+
+def impedance(*vectors):
+    return 1 / sum(complex(v[1] / v[0]) for v in vectors)
+
+
+# for printing
+
+def polar(x):
+    return "%.4f / %.4f" % (np.absolute(x), np.angle(x, deg=True))
+
+def status(v, note=0):
+    print("p({}) = {:.4f}".format(note, power(v)))
+    print("z({}) = {:.4f}".format(note, complex(v[0]/v[1])))
+    print("i({}) = {}".format(note, polar(v[1])))
+    print("e({}) = {}".format(note, polar(v[0])))
+    print()
+
+def component(values, fd, precision=4):
+    return [ notation(component_value(z, fd), precision) for z in values ]
+
+def notation(x, precision=4):
+    SUFFIX = ["p", "n", "u", "m", "", "k", "M", "G"]
+    exp = np.floor(np.log10(np.absolute(x)))
+    mant = round(x / 10**exp, precision-1)
+    p = int(exp // 3)
+    value = (mant * 10**exp) / 10**(3 * p)
+    return "%g%s%s" % (np.absolute(value), SUFFIX[p-4], 'F' if x < 0 else 'H')
+
